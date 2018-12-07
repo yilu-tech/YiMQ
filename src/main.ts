9687c30d63c7b0ef870a9f814926c4f55a2e0f0f
lishu <lishu@yilu.co>
2018-12-07 16:06:01 +0800

    使用Fastify作为底层框架

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { FastifyAdapter } from '@nestjs/core/adapters/fastify-adapter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule,new FastifyAdapter());
   await app.listen(3000);
 }
 bootstrap();
