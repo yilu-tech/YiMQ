68c940b127c050d803ade0b7afa416bf307f3209
lishu <lishu@yilu.co>
2019-07-06 14:55:09 +0800

    遗漏的清理

--- src/Core/Job/QueueJob.ts
+++ src/Core/Job/QueueJob.ts
@@ -1,6 +1,6 @@
 import { Job } from "./Job";
-import { JobReceiver } from "./interfaces/JobReceiver";
+
 
 export class QueueJob extends Job{
-    public readonly receiver:JobReceiver;
+    public readonly receiver;
 }
