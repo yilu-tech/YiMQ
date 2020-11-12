c8d8dd504825a188c08e5e6bc590907587d42511
lishu <lishu@yilu.co>
2020-11-12 20:57:50 +0800

    fix admin/home api redis memory info bug

--- src/Handlers/redis/RedisClient.ts
+++ src/Handlers/redis/RedisClient.ts
@@ -11,11 +11,23 @@ export class RedisClient extends Ioredis{
             uptime_in_seconds: info.match(/uptime_in_seconds:(.*)/)[1],
             uptime_in_days: info.match(/uptime_in_days:(.*)/)[1],
         }
+        let maxmemoryMatch = info.match(/maxmemory:(.*)/)
+        let total_system_memory;
+        let total_system_memory_human;
+        
+        if(maxmemoryMatch && Number(maxmemoryMatch[1]) != 0 ){
+            total_system_memory = maxmemoryMatch[1]
+            total_system_memory_human = info.match(/maxmemory_human:(.*)/)[1];
+        }else{
+            total_system_memory = info.match(/total_system_memory:(.*)/)[1]
+            total_system_memory_human = info.match(/total_system_memory_human:(.*)/)[1]
+        }
+        
         let memory:any = {
             used_memory: info.match(/used_memory:(.*)/)[1],
             used_memory_human: info.match(/used_memory_rss_human:(.*)/)[1],
-            total_system_memory: info.match(/total_system_memory:(.*)/)[1],
-            total_system_memory_human: info.match(/total_system_memory_human:(.*)/)[1],
+            total_system_memory: total_system_memory,
+            total_system_memory_human: total_system_memory_human,
         }
         memory.used_memory_total_system_memory_perc = `${(memory.used_memory/memory.total_system_memory*100).toFixed(2)}%`
 
