fc0ad306962ae89e7c1a9afec97db610c9543f98
leekeal <lishu@yilu.co>
2021-11-07 00:26:52 +0800

    fix command

--- src/Commands/Core/clear_abnormal_job.ts
+++ src/Commands/Core/clear_abnormal_job.ts
@@ -1,9 +1,5 @@
 
 import {App} from "../CommandApp";
-import { MessageStatus, MessageClearStatus } from "../../Constants/MessageConstants";
-import { Actor } from "../../Core/Actor";
-import { id } from "date-fns/locale";
-import { ActorStatus } from "../../Constants/ActorConstants";
 
 export async function clear_abnormal_job(){
     console.log('migrate to clear_abnormal_job')
@@ -11,43 +7,13 @@ export async function clear_abnormal_job(){
     console.time('time:')
     await app.initConfig();
     await app.initContext();
-    await clearJobIndex(app);
     await clear(app);
     await app.closeContext()
     console.timeEnd('time:')
 }
 
-async function clearJobIndex(app:App) {
-    console.log('---------- clearJobIndex')
-    let actors = app.actorManager.actors;
-    let statusItems = ['completed','waiting','active','delayed','failed','paused']
-    for (const [id,actor] of actors) {
-
-        
-        let idsTotal = 0;
-        for (const statusItem of statusItems) {
-            idsTotal += await clearJobStatusIndex(actor,statusItem)
-        }
-        console.log(actor.id,actor.name,idsTotal)
-    }
-    
-}
-
-async function clearJobStatusIndex(actor,status) {
-    let ids = await actor.redisClient.zrange(`bull:${actor.id}:${status}`,0,-1);
-    for (const jobId of ids) {
-        if(await actor.redisClient.exists(`bull:${actor.id}:${jobId}`) == 0){
-            await actor.redisClient.zrem(`bull:${actor.id}:${status}`,jobId)
-            console.log('clear',actor.id,actor.name,jobId);
-        }           
-    }
-    return ids.length;
-}
-
-
 async function clear(app:App){
 
-    console.log('---------- clear')
     let actors = app.actorManager.actors;
     for (const [id,actor] of actors) {
         let abnormal_jobs = []
